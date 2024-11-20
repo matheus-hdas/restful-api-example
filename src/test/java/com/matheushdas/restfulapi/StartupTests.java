a12bf2048a7852f8950b6d61fad2868b8a12695d
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-20 15:20:37 -0300

    first commit

---
+++ src/test/java/com/matheushdas/restfulapi/StartupTests.java
@@ -0,0 +1,13 @@
+package com.matheushdas.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
