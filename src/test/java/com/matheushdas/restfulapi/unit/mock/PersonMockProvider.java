696c89f9661a1130b0a973b3c1e2dc6cb3c066a5
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-21 13:16:09 -0300

    test: add unit test to `PersonService` with mockito and junit to test hateoas link generation

---
+++ src/test/java/com/matheushdas/restfulapi/unit/mock/PersonMockProvider.java
@@ -0,0 +1,74 @@
+package com.matheushdas.restfulapi.unit.mock;
+
+import com.matheushdas.restfulapi.dto.CreatePersonRequest;
+import com.matheushdas.restfulapi.dto.PersonResponse;
+import com.matheushdas.restfulapi.dto.UpdatePersonRequest;
+import com.matheushdas.restfulapi.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonMockProvider {
+    public Person mockEntity() {
+        return mockEntity(0);
+    }
+
+    public PersonResponse mockVO() {
+        return mockVO(0);
+    }
+
+    public List<Person> mockEntityList() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<PersonResponse> mockVOList() {
+        List<PersonResponse> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockVO(i));
+        }
+        return persons;
+    }
+
+    public Person mockEntity(Integer number) {
+        return new Person(
+                number.longValue(),
+                "First Name Test" + number,
+                "Last Name Test" + number,
+                "Address Test" + number,
+                ((number % 2)==0) ? "Male" : "Female"
+        );
+    }
+
+    public CreatePersonRequest mockCreateRequest(Integer number) {
+        return new CreatePersonRequest(
+                "First Name Test" + number,
+                "Last Name Test" + number,
+                "Address Test" + number,
+                ((number % 2)==0) ? "Male" : "Female"
+        );
+    }
+
+    public UpdatePersonRequest mockUpdateRequest(Integer number) {
+        return new UpdatePersonRequest(
+                number.longValue(),
+                "First Name Test" + number,
+                "Last Name Test" + number,
+                "Address Test" + number,
+                ((number % 2)==0) ? "Male" : "Female"
+        );
+    }
+
+    public PersonResponse mockVO(Integer number) {
+        return new PersonResponse(
+                number.longValue(),
+                "First Name Test" + number,
+                "Last Name Test" + number,
+                "Address Test" + number,
+                ((number % 2)==0) ? "Male" : "Female"
+        );
+    }
+}
---
+++ src/test/java/com/matheushdas/restfulapi/unit/service/PersonServiceTest.java
@@ -0,0 +1,143 @@
+package com.matheushdas.restfulapi.unit.service;
+
+import com.matheushdas.restfulapi.dto.CreatePersonRequest;
+import com.matheushdas.restfulapi.dto.PersonResponse;
+import com.matheushdas.restfulapi.dto.UpdatePersonRequest;
+import com.matheushdas.restfulapi.mapper.PersonMapper;
+import com.matheushdas.restfulapi.model.Person;
+import com.matheushdas.restfulapi.repository.PersonRepository;
+import com.matheushdas.restfulapi.service.PersonService;
+import com.matheushdas.restfulapi.unit.mock.PersonMockProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+    private PersonMockProvider input;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private PersonMapper personMapper;
+
+    @BeforeEach
+    void setUp() {
+        input = new PersonMockProvider();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void shouldReturnListPersonResponseWithHateoasLink_whenFindAll() {
+        List<Person> people = input.mockEntityList();
+
+        when(personRepository.findAll()).thenReturn(people);
+        when(personMapper.toResponseList(people)).thenReturn(input.mockVOList());
+
+        int i = 0;
+
+        for(PersonResponse result : personService.findAll()) {
+            assertNotNull(result);
+            assertNotNull(result.getKey());
+            assertNotNull(result.getLinks());
+
+            assertTrue(result.toString().contains("links: [</api/person/" + i + ">;rel=\"self\"]"));
+
+            assertEquals(people.get(i).getId(), result.getKey());
+            assertEquals(people.get(i).getFirstName(), result.getFirstName());
+            assertEquals(people.get(i).getLastName(), result.getLastName());
+            assertEquals(people.get(i).getAddress(), result.getAddress());
+            assertEquals(people.get(i).getGender(), result.getGender());
+            i++;
+        }
+    }
+
+    @Test
+    void shouldReturnPersonResponseWithHateoasLink_whenFindById() {
+        Person person = input.mockEntity(1);
+
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+        when(personMapper.toResponse(person)).thenReturn(input.mockVO(1));
+
+        PersonResponse result = personService.findById(1L);
+
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</api/person/1>;rel=\"self\"]"));
+
+        assertEquals(person.getId(), result.getKey());
+        assertEquals(person.getFirstName(), result.getFirstName());
+        assertEquals(person.getLastName(), result.getLastName());
+        assertEquals(person.getAddress(), result.getAddress());
+        assertEquals(person.getGender(), result.getGender());
+    }
+
+    @Test
+    void shouldCreateEntityAndSendPersonResponseWithHateoasLink_whenSave() {
+        CreatePersonRequest request = input.mockCreateRequest(1);
+        Person person = input.mockEntity(1);
+
+
+        when(personMapper.toEntity(request)).thenReturn(person);
+        when(personRepository.save(person)).thenReturn(person);
+        when(personMapper.toResponse(person)).thenReturn(input.mockVO(1));
+
+        PersonResponse result = personService.save(request);
+
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</api/person/1>;rel=\"self\"]"));
+
+        assertEquals(person.getId(), result.getKey());
+        assertEquals(person.getFirstName(), result.getFirstName());
+        assertEquals(person.getLastName(), result.getLastName());
+        assertEquals(person.getAddress(), result.getAddress());
+        assertEquals(person.getGender(), result.getGender());
+    }
+
+    @Test
+    void shouldPatchEntityAndSendPersonResponseWithHateoasLink_whenUpdate() {
+        UpdatePersonRequest request = input.mockUpdateRequest(1);
+        Person person = input.mockEntity(1);
+
+
+        when(personRepository.existsById(request.id())).thenReturn(true);
+        when(personMapper.toEntity(request)).thenReturn(person);
+        when(personRepository.save(person)).thenReturn(person);
+        when(personMapper.toResponse(person)).thenReturn(input.mockVO(1));
+
+        PersonResponse result = personService.update(request);
+
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</api/person/1>;rel=\"self\"]"));
+
+        assertEquals(person.getId(), result.getKey());
+        assertEquals(person.getFirstName(), result.getFirstName());
+        assertEquals(person.getLastName(), result.getLastName());
+        assertEquals(person.getAddress(), result.getAddress());
+        assertEquals(person.getGender(), result.getGender());
+    }
+}
