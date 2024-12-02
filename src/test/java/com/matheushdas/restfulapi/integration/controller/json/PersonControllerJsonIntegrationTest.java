ff40b913950163b1b6b59062c05626fa04882022
matheus-hdas <matheussantos.betim@gmail.com>
2024-12-01 23:57:53 -0300

    test: create PersonController integration test

--- src/test/java/com/matheushdas/restfulapi/integration/controller/json/PersonControllerJsonIntegrationTest.java
+++ src/test/java/com/matheushdas/restfulapi/integration/controller/json/PersonControllerJsonIntegrationTest.java
@@ -8,6 +8,7 @@ import com.matheushdas.restfulapi.dto.auth.LoginRequest;
 import com.matheushdas.restfulapi.dto.auth.LoginResponse;
 import com.matheushdas.restfulapi.dto.person.CreatePersonRequest;
 import com.matheushdas.restfulapi.dto.person.PersonResponse;
+import com.matheushdas.restfulapi.dto.person.UpdatePersonRequest;
 import com.matheushdas.restfulapi.integration.container.ContainerEngine;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
@@ -28,6 +29,7 @@ public class PersonControllerJsonIntegrationTest extends ContainerEngine {
     private static ObjectMapper objectMapper;
 
     private static CreatePersonRequest createPersonRequest;
+    private static UpdatePersonRequest updatePersonRequest;
 
     @BeforeAll
     public static void setUp() {
@@ -90,7 +92,85 @@ public class PersonControllerJsonIntegrationTest extends ContainerEngine {
 
     @Test
     @Order(3)
-    public void shouldReturnInvalidCorsRequest_whenCreatePersonWithInvalidOrigin() {
+    public void shouldReturnPersonResponseWithHisId_whenFindById() throws JsonProcessingException {
+
+        String body = given().spec(specification)
+                .header(TestContextConfig.ORIGIN_HEADER_PARAM, "http://localhost:3000")
+                .contentType(JSON)
+                .pathParam("id", 1L)
+                .when().get("{id}")
+                .then().statusCode(200)
+                .extract().body().asString();
+
+        PersonResponse foundedPerson = objectMapper.readValue(body, PersonResponse.class);
+
+        assertNotNull(foundedPerson);
+        assertNotNull(foundedPerson.getKey());
+        assertNotNull(foundedPerson.getFirstName());
+        assertNotNull(foundedPerson.getLastName());
+        assertNotNull(foundedPerson.getAddress());
+        assertNotNull(foundedPerson.getGender());
+
+        assertEquals(1L, foundedPerson.getKey());
+        assertEquals("TestFirstName", foundedPerson.getFirstName());
+        assertEquals("TestLastName", foundedPerson.getLastName());
+        assertEquals("TestAddress", foundedPerson.getAddress());
+        assertEquals("Male", foundedPerson.getGender());
+    }
+
+    @Test
+    @Order(4)
+    public void shouldReturnUpdatedPersonResponse_whenUpdatePerson() throws JsonProcessingException {
+        mockUpdateRequest();
+
+        String body = given().spec(specification)
+                .header(TestContextConfig.ORIGIN_HEADER_PARAM, "http://localhost:3000")
+                .contentType(JSON)
+                .body(updatePersonRequest)
+                .when().put()
+                .then().statusCode(200)
+                .extract().body().asString();
+
+        PersonResponse updatedPerson = objectMapper.readValue(body, PersonResponse.class);
+
+        assertNotNull(updatedPerson);
+        assertNotNull(updatedPerson.getKey());
+        assertNotNull(updatedPerson.getFirstName());
+        assertNotNull(updatedPerson.getLastName());
+        assertNotNull(updatedPerson.getAddress());
+        assertNotNull(updatedPerson.getGender());
+
+        assertTrue(updatedPerson.getKey() > 0);
+
+        assertEquals("UpdatedTestFirstName", updatedPerson.getFirstName());
+        assertEquals("UpdatedTestLastName", updatedPerson.getLastName());
+        assertEquals("UpdatedTestAddress", updatedPerson.getAddress());
+        assertEquals("Male", updatedPerson.getGender());
+    }
+
+    @Test
+    @Order(5)
+    public void shouldDeletePersonWithGivenIdRecord_whenDeletePerson() {
+        given().spec(specification)
+                .header(TestContextConfig.ORIGIN_HEADER_PARAM, "http://localhost:3000")
+                .contentType(JSON)
+                .pathParam("id", 1L)
+                .when().delete("{id}")
+                .then().statusCode(204)
+                .extract().body().asString();
+
+        given().spec(specification)
+                .header(TestContextConfig.ORIGIN_HEADER_PARAM, "http://localhost:3000")
+                .contentType(JSON)
+                .pathParam("id", 1L)
+                .when().get("{id}")
+                .then().statusCode(404)
+                .extract().body().asString();
+    }
+
+    @Test
+    @Order(6)
+    public void shouldReturnInvalidCorsRequest_whenAnyRequestWithInvalidOrigin() {
         String body = given().spec(specification)
                 .header(TestContextConfig.ORIGIN_HEADER_PARAM, "http://invalidorigin.com")
                 .contentType(JSON)
@@ -109,6 +189,16 @@ public class PersonControllerJsonIntegrationTest extends ContainerEngine {
                "TestLastName",
                "TestAddress",
                "Male"
+        );
+    }
+
+    private void mockUpdateRequest() {
+        updatePersonRequest = new UpdatePersonRequest(
+                1L,
+                "UpdatedTestFirstName",
+                "UpdatedTestLastName",
+                "UpdatedTestAddress",
+                "Male"
         );
     }
 }
