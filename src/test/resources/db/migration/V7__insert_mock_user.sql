3fe4ca1b2ccefd46d942aa43c9c0d2ab4317935e
matheus-hdas <matheussantos.betim@gmail.com>
2024-12-01 22:24:37 -0300

    test: add migration to create mocked user for tests

---
+++ src/test/resources/db/migration/V7__insert_mock_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_user
+(id, username, full_name, email, password, account_non_expired, account_non_locked, credentials_non_expired, enabled)
+VALUES
+(1, 'TestUsername', 'TestFullName', 'testemail@test.com', '$2a$10$HLX39HbrRFYiz2JsxyLf9Oz/1oOlyjz/OYWkvlCokHMmOF5tI38pi', 1, 1, 1, 1);
